1052e5840a00ef08b2f34b4178c7a4e747364d80
fas2037 <fas2037@thi.de>
2021-06-16 18:00:20 +0200

    commit 2

--- RTP_V5_Regelung_DKL_Test_PIDT1_Regler.m
+++ RTP_V5_Regelung_DKL_Test_PIDT1_Regler.m
@@ -77,46 +77,42 @@ Gr = K;
 % Weiter mit Abschnitt "WOK zeichnen und Reglerverstärkung K bestimmen"
 
 %% Auslegung PIDT1-Regler
-% %  Zur Aktivierung des Blocks alle Zeilen des Blocks markieren und "Uncomment" drücken
-% % ***** PIDT1-Regler, Kr = 1, Tn = 0.1323, Tv = 0.0242, T = 0.004 => Nullstellen bei -10 und -30
-% 
-% T = 0.004;   %Parasitaere Zeitkonstante, nicht aendern!
-% 
-% % Vorgabe der Nullstellen bei -10 und -30
-% % Uebertragungsfunktion des PIDT1-Reglers
-% % Zaehler faktorisiert mit Tz1, Tz2        und  ausmultipliziert mit T, Tv und Tn
-% %         Kr(Tz1*s + 1)*(Tz2*s + 1)             Kr((T*Tn+Tn*Tv)*s^2 + (T+Tn)*s + 1)
-% %Gr(s) = ----------------------------   =   ---------------------------------------
-% %          Tn*s*(T*s + 1)                         Tn*s*(T*s + 1)
-% 
-% Tz1=1/10;   % Soll dominierende Nullstelle festlegen -> wichtig für Vorfilter
-% Tz2=1/30;
-% 
-% Tn = Tz1 + Tz2 - T;   % Berechnungsformel eingeben, nicht Zahlenwert!
-% Tv = Tz1*Tz2 / Tn;   % Berechnungsformel eingeben, nicht Zahlenwert!
-% 
-% % Kontrolle der Parameter
-% %  Tn = 0.1293, Tv = 0.0218
-% 
-% % Parametrierung der Regler-Uefkt fuer Simulation der Sprungantwort
-% zr = [T*Tn+Tn*Tv T+Tn 1]; 
-% nr = [T*Tn Tn 0];
-% % % ***********************
-% 
-% Gr = tf(zr, nr);           % Übertragungsfunktion des Reglers
-% 
-% % ***** Ende PIDT1-Regler *****
+%  Zur Aktivierung des Blocks alle Zeilen des Blocks markieren und "Uncomment" drücken
+% ***** PIDT1-Regler, Kr = 1, Tn = 0.1323, Tv = 0.0242, T = 0.004 => Nullstellen bei -10 und -30
 
-%%  WOK zeichnen und Reglerverstärkung K bestimmen
+T = 0.004;   %Parasitaere Zeitkonstante, nicht aendern!
+
+% Vorgabe der Nullstellen bei -10 und -30
+% Uebertragungsfunktion des PIDT1-Reglers
+% Zaehler faktorisiert mit Tz1, Tz2        und  ausmultipliziert mit T, Tv und Tn
+%         Kr(Tz1*s + 1)*(Tz2*s + 1)             Kr((T*Tn+Tn*Tv)*s^2 + (T+Tn)*s + 1)
+%Gr(s) = ----------------------------   =   ---------------------------------------
+%          Tn*s*(T*s + 1)                         Tn*s*(T*s + 1)
+
+Tz1=1/10;   % Soll dominierende Nullstelle festlegen -> wichtig für Vorfilter
+Tz2=1/30;
+
+Tn = Tz1 + Tz2 - T;   % Berechnungsformel eingeben, nicht Zahlenwert!
+Tv = Tz1*Tz2 / Tn - T;   % Berechnungsformel eingeben, nicht Zahlenwert!
+
+% Kontrolle der Parameter
+%  Tn = 0.1293, Tv = 0.0218
 
 % Parametrierung der Regler-Uefkt fuer Simulation der Sprungantwort
-zkomp = 10; 
-nkomp = [1 10];
+zr = [T*Tn+Tn*Tv T+Tn 1]; 
+nr = [T*Tn Tn 0];
 % % ***********************
 
-%G_komp = tf(zkomp, nkomp);
+Gr = tf(zr, nr);           % Übertragungsfunktion des Reglers
 
-%Go = G_komp * G_s * Gr;           % offener Kreis mit Regler
+% ***** Ende PIDT1-Regler *****
+
+%%  WOK zeichnen und Reglerverstärkung K bestimmen
+zf = 1;
+nf = [Tz1 1];
+
+Gfilt = tf(zf, nf);
+
 Go = G_s * Gr;
 %minreal(Go)             % bessere Darstellung der Übertragungsfunktion
 zpk(Go)                  % Darstellung in faktorisierter WO-NF
@@ -132,7 +128,7 @@ grid on;
 
 % Ablesen der gewünschten Reglerverstärkung aus der WOK
 % für PIDT1 gilt: Pole am Verzweigungspunkt zwischen den Nullstellen 
-K = 0.0509;                         % Reglerverstärkung aus WOK, dominierende Pole auf Verzweigungspunkt
+K = 0.0508;                         % Reglerverstärkung aus WOK, dominierende Pole auf Verzweigungspunkt
 Gw = feedback(Go*K, 1);       % Übertragungsfunktion des geschlossenen Regelkreises
 
 % Erweiterung des Regelkreises (Aufgabe 2.4) hier vornehmen
